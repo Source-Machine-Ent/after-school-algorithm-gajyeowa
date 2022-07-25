3c9cfa81c8d24752805e2612df38a865698b100c
210_reverof <sheltonwon@gmail.com>
2022-07-25 19:16:31 +0900

    신고 결과 받기

---
+++ 210/level1/신고결과받기.java
@@ -0,0 +1,41 @@
+package level1;
+// 20220725
+
+import java.util.*;
+
+class Solution {
+    static List<HashSet<Integer>> reports = new ArrayList<>();
+
+    public int[] solution(String[] id_list, String[] report, int k) {
+
+        Map<String, Integer> ids = new HashMap<>();
+        for (int i = 0; i < id_list.length; i++)
+            ids.put(id_list[i], i);
+
+        reports.clear();
+        for (int i = 0; i < id_list.length; i++)
+            reports.add(new HashSet<Integer>());
+
+        StringTokenizer st;
+        String userId, reportedId;
+        for (String str : report) {
+            st = new StringTokenizer(str);
+            userId = st.nextToken();
+            reportedId = st.nextToken();
+
+            reports.get(ids.get(reportedId)).add(ids.get(userId));
+        }
+
+        int[] answer = new int[id_list.length];
+
+        for (int i = 0; i < id_list.length; i++) {
+            if (reports.get(i).size() >= k) {
+                for (int r : reports.get(i))
+                    answer[r]++;
+            }
+        }
+
+        return answer;
+    }
+
+}
