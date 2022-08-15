48817728554f6de560105d1328939120d25000c8
210_reverof <sheltonwon@gmail.com>
2022-08-15 22:36:44 +0900

    programmers 숫자문자열과영단어.java

---
+++ 210/level1/숫자문자열과영단어.java
@@ -0,0 +1,21 @@
+package level1;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        String s = "one4seveneight";
+
+        Solution sol = new Solution();
+        System.out.println("result : " + sol.solution(s));
+    }
+}
+
+
+class Solution {
+    public int solution(String s) {
+        String arr[]={"zero","one","two","three","four","five","six","seven","eight","nine"};
+        for(int i=0;i<10;i++){
+            s=s.replace(arr[i],Integer.toString(i));
+        }
+        return Integer.parseInt(s);
+    }
+}
