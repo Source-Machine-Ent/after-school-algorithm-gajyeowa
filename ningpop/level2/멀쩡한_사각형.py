1c83519ebddc9adc8b8ca53f9641f8f80c1f8d0e
ningpop <craft1933@naver.com>
2022-08-27 01:24:11 +0900

    [#70] 멀쩡한 사각형 - 정답

--- ningpop/level2/멀쩡한_사각형.py
+++ ningpop/level2/멀쩡한_사각형.py
@@ -1,16 +1,18 @@
 # 2022.08.26
-# 풀이 시간: 분 초
-# 채점 결과: 
-# 시간복잡도: 
+# 풀이 시간: 48분 28초
+# 채점 결과: 오답 -> 정답
+# 시간복잡도: O(1)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/62048
 
 import math
 
 def solution(w: int, h: int) -> int:
-    inclination = abs(h / w)
-    block = math.ceil(inclination) * w
-    answer = w * h - block
+    gcd_num = math.gcd(w, h)
+    w_prime = w // gcd_num
+    h_prime = h // gcd_num
+    answer = w * h - ((w_prime + h_prime - 1) * gcd_num)
     return answer
 
 print(solution(8, 12)) # 80
 print(solution(3, 4)) # 6
+print(solution(3, 5)) # 8
