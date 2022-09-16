cfcfc2288c63d7f8f4b81159270f8eab9e4d4729
210_reverof <sheltonwon@gmail.com>
2022-09-16 10:48:10 +0900

    programmers 정수삼각형

--- 210/level3/정수삼각형.java
+++ 210/level3/정수삼각형.java
@@ -3,9 +3,6 @@
 package level3;
 
 class Main {
-    static int N, M;
-    static int[] Group;
-
     public static void main(String[] args) throws Exception {
         int[][] triangle = { { 7 }, { 3, 8 }, { 8, 1, 0 }, { 2, 7, 4, 4 }, { 4, 5, 2, 6, 5 } };
 
